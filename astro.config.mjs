c924669f345a0acbef4099e87b471fbb8e958a51
Waylon Baumgardner <waylonrobert@gmail.com>
2024-07-25 09:18:27 -0700

    fix timezone issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,24 @@
 // import { defineConfig } from 'astro/config';
 import { defineConfig, envField } from 'astro/config';
 
-// https://astro.build/config
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
-    experimental: {
-      env: {
-        schema: {
-        AIRTABLE_API_URL: envField.string({ context: "server", access: "secret" }),
-        AIRTABLE_API_KEY: envField.string({ context: "server", access: "secret" }),
-        }
+  experimental: {
+    env: {
+      schema: {
+        AIRTABLE_API_URL: envField.string({
+          context: "server",
+          access: "secret"
+        }),
+        AIRTABLE_API_KEY: envField.string({
+          context: "server",
+          access: "secret"
+        })
       }
     }
-  })
+  },
+  output: "server",
+  adapter: netlify()
+});
